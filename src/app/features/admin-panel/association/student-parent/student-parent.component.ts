fea6584e87db89bbb397c29bc356e46254e3e896
Erish Berboso <erishberboso10@gmail.com>
2026-04-07 22:34:55 +0800

    chore[admin] association page added

---
+++ src/app/features/admin-panel/association/student-parent/student-parent.component.ts
@@ -0,0 +1,10 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-student-parent',
+  standalone: true,
+  templateUrl: './student-parent.component.html',
+})
+export class StudentParentComponent {
+  
+}
---
+++ src/app/features/admin-panel/association/student-schedule/student-schedule.component.ts
@@ -0,0 +1,10 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-student-schedule',
+  standalone: true,
+  templateUrl: './student-schedule.component.html',
+})
+export class StudentScheduleComponent {
+  
+}
