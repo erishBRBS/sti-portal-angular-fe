ba119609405503cfe555225886e6d41886983abd
Arvin Cabunoc <engr3.arvincabunoc@gmail.com>
2026-04-09 14:29:35 +0800

    [STI-17] - add implement face recognation

---
+++ src/app/features/admin-panel/association/student-face-id/student-face-id.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sti-student-face-id',
+  standalone: true,
+  imports: [
+
+  ],
+  templateUrl: './student-face-id.component.html',
+})
+export class StudentFaceIdComponent {
+
+}
