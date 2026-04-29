c0152ad9615a346c2e47af1875aacf7d000d4c11
Erish Berboso <erishberboso10@gmail.com>
2026-04-29 11:08:21 +0800

    chore [admin] contact_number student fixed

--- src/app/payloads/admin-panel/user-management/student/create-student.payload.ts
+++ src/app/payloads/admin-panel/user-management/student/create-student.payload.ts
@@ -2,14 +2,14 @@ export interface CreateStudentPayload {
   first_name: string;
   middle_name?: string;
   last_name: string;
-  email: string;
   student_no: string;
-  mobile_number?: string;
+  email: string;
+  contact_number?: string;
   course_id: number;
   section_id: number;
-  year_level?: string; 
-  username?: string;     
-  rfid_code?: string; 
+  year_level?: string;
+  username?: string;
+  rfid_code?: string;
 }
 export interface DeleteStudentPayload {
   id: number[];
